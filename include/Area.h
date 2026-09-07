bdcf59fe5775b9d69057dd0ac82911974edb3dcf
YKwagga <yoshua.h.smit@gmail.com>
2026-09-07 11:46:26 +0200

    Updated area

--- include/Area.h
+++ include/Area.h
@@ -2,12 +2,15 @@
 #define AREA_H
 
 #include "FilmElement.h"
+#include <functional>
 #include <vector>
 
 class Area : public FilmElement {
 public:
     explicit Area(const std::string& name,
-                  const std::shared_ptr<std::size_t>& versionToken = std::shared_ptr<std::size_t>());
+                  const std::shared_ptr<std::size_t>& versionToken = std::shared_ptr<std::size_t>(),
+                  const std::shared_ptr<std::function<void()> >& mutationObserver =
+                      std::shared_ptr<std::function<void()> >());
     virtual ~Area() {}
 
     void add(std::unique_ptr<FilmElement> child);
@@ -21,11 +24,14 @@ public:
     void print(std::size_t depth) const;
     std::shared_ptr<std::size_t> versionToken() const;
     void appendChildren(std::vector<FilmElement*>& output) const;
+    void setMutationContext(const std::shared_ptr<std::size_t>& versionToken,
+                            const std::shared_ptr<std::function<void()> >& mutationObserver);
 
 private:
     std::string areaName;
     std::vector<std::unique_ptr<FilmElement> > children;
     std::shared_ptr<std::size_t> version;
+    std::shared_ptr<std::function<void()> > observer;
 };
 
 #endif
